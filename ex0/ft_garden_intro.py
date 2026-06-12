ea5c9855a22e7aca9087785c73dc3c2ca1f8cac4
Daniel Mateus Campos Marques <danicamp@c1r9s3.42porto.com>
2026-06-12 14:39:25 +0100

    first commit

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_intro.py                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: danicamp <danicamp@student.42porto.com>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/06/08 15:45:24 by danicamp          #+#    #+#              #
+#    Updated: 2026/06/08 15:59:30 by danicamp         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_garden_intro():
+    plant = "Rose"
+    height = "25cm"
+    age = "30 days"
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
